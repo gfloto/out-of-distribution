350c392dfe3ccaffae1446edc8fc29fe8b6d1133
griffin_floto <gfloto@uoguelph.ca>
2023-08-17 21:00:40 -0400

    2d normalized pdf

--- math/pdf.py
+++ math/pdf.py
@@ -21,29 +21,58 @@ def phi(x):
     return a/xn * x 
 
 def norm_const(x, v):
-    n = x.shape[1]
+    d = x.shape[1]
     xn = x.norm(dim=1)
 
-    d = (1 - xn.pow(2)) *  v.prod(dim=1) * np.float_power(2*np.pi, n/2)
+    num = ( (1 + xn).log() - (1 - xn).log() ).pow(d-1)
+    denom = (1 - xn.pow(2)) * xn.pow(d-1) * v.prod(dim=1).sqrt() * np.float_power(2*np.pi, d/2)
 
-    return 2 / d
+    return 2 * num / denom
+
+def monte_integrate(y, d):
+    if d == 1:
+        area = 2
+    elif d == 2:
+        area = np.pi
+
+    return area / y.shape[0] * y.sum()
 
 if __name__ == '__main__':
-    n = 200
-    d = 1
+    n = 50
+    d = 2
 
-    x = torch.linspace(-1, 1, n).view(-1, d)
-    v = torch.randn(1, d).abs()
+    # cartension product
+    x = torch.linspace(-1, 1, n)
+    x = torch.cartesian_prod(x, x)
+    x = x[x.norm(dim=1) < 1]
+
+    # random parameters for gaussian ball
     mu = 2*torch.rand(1, d) - 1
-
-    print(mu, v)
+    v = torch.ones(1, d).abs()
+    v[0, 0] = v[0, 1]
+    print(f'std: {v.sqrt().numpy()}, mu: {mu.numpy()}')
 
     y = gauss_ball(x, mu, v)
-    x = x.squeeze()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(x, y)
-    ax.set_aspect('equal')
-    plt.show()
+    # print max point
+    print(f'Max: {y.max().numpy()} at {x[y.argmax()].numpy()}')
+
+    # monte carlo integration
+    integral = monte_integrate(y, d)
+    print(f'Integral: {integral:.4f}')
+
+    if x.shape[1] == 2:
+        # plot gaussian ball using 3d plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        # x : [n, 2], y : [n]
+        ax.plot_trisurf(x[:, 0], x[:, 1], y, cmap='twilight_shifted', edgecolor='none')
+        plt.show()
+    elif x.shape[1] == 1:
+        # plot gaussian ball using 2d plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(x[:, 0], y, s=3)
+        ax.axis('equal')
+        plt.show()
 
